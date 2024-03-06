7e35c9e8d4f5ad35c6b07137ea8e137e57140bc9
sunny patel <sunnykpatel.skp@gmail.com>
2024-03-06 18:24:02 -0500

    add C sharp lessons

---
+++ CsharpProjects/DecisionChallenge/Program.cs
@@ -0,0 +1,24 @@
+﻿Random random = new Random();
+
+int daysUntilExpiration = random.Next(12);
+
+int discountPercentage = 0;
+
+if (daysUntilExpiration <= 10)
+{
+    if (daysUntilExpiration <= 5)
+    {
+        discountPercentage = 10;
+        Console.WriteLine($"Your subscription will expire in {daysUntilExpiration} days./n Renew now and save {discountPercentage}%!")
+    }
+    else if (daysUntilExpiration == 1)
+    {
+        discountPercentage = 20;
+        Console.WriteLine($"Your subscription will expire in within a day./n Renew now and save {discountPercentage}%!")
+    }
+    else if (daysUntilExpiration == 0)
+    {
+        Console.WriteLine($"Your subscription expired")
+    }
+    Console.WriteLine($"Your subscription will expire soon. Renew now!")
+}
---
+++ CsharpProjects/TestProject/Program.cs
@@ -0,0 +1,8 @@
+﻿
+int firstValue = 500;
+int secondValue = 600;
+int largerValue;
+
+largerValue = Math.Max(firstValue,secondValue);
+
+Console.WriteLine(largerValue);
